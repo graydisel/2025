2900876a55729129c2e6e861dd287cbb4c0f57e0
Babanenkov Sergii <graydisel@gmail.com>
2025-01-31 14:37:36 +0200

    Complete Home work 22

--- lesson_22/home-work-22/Main.js
+++ lesson_22/home-work-22/Main.js
@@ -1,3 +1,4 @@
+'use strict';
 console.log('#5. JavaScript homework example file')
 
 /*
@@ -116,27 +117,28 @@ console.log(myMax(list)); // 233
  *
  * Створіть функцію myMul(a, b), яка буде множити числа а і b, повертаючи результат.
  */
-
-// const myMul = () => {}
-
+console.log('#5')
+const myMul = (a, b) => {
+    return a * b;
+}
 /*
  * Створіть функції myDouble(n), яка приймає один параметр і подвоює його.
  * Використовувати множення або інші математичні операції всередині функції - заборонено, тільки bind() і myMul().
  * Функція повертає результат обчислення.
  */
 
-// const myDouble
+const myDouble =  myMul.bind( undefined, 2);
 
-// console.log(myDouble(3)) // = myMul(2, 3) = 6
-// console.log(myDouble(4)) // = myMul(2, 4) = 8
-// console.log(myDouble(5)) // = myMul(2, 5) = 10
+console.log(myDouble(3)) // = myMul(2, 3) = 6
+console.log(myDouble(4)) // = myMul(2, 4) = 8
+console.log(myDouble(5)) // = myMul(2, 5) = 10
 
 // Аналогічним чином створюємо функцію myTriple(n), яка потроює параметр, що приймає, повертаючи результат.
 
-// const myTriple
+const myTriple = myMul.bind (undefined, 3);
 
-// console.log(myTriple(3)) // = myMul(3, 3) = 9
-// console.log(myTriple(4)) // = myMul(3, 4) = 12
-// console.log(myTriple(5)) // = myMul(3, 5) = 15
+console.log(myTriple(3)) // = myMul(3, 3) = 9
+console.log(myTriple(4)) // = myMul(3, 4) = 12
+console.log(myTriple(5)) // = myMul(3, 5) = 15
 
-// export { counter, counterFactory, myPow, myMax, myMul, myDouble, myTriple }
+export { counter, counterFactory, myMax, myMul, myDouble, myTriple }
