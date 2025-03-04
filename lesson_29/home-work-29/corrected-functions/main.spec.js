0fa0c8919a578fe8b12641bdbbb76be99597e4e0
Sergii Babanenkov <graydisel@gmail.ru>
2025-03-04 10:52:06 +0200

    Complete Homework 29

--- lesson_29/home-work-29/corrected-functions/main.spec.js
+++ lesson_29/home-work-29/corrected-functions/main.spec.js
@@ -1,48 +1,115 @@
 import {ageClassification, weekFn} from "./main.js";
-describe('ageClassification', () => {
-    it('Should display age type, depending from age. ', () => {
-        expect(ageClassification(-1)).toBe(null);
-        expect(ageClassification(0)).toBe(null);
-        expect(ageClassification(1)).toBe('Дитинство');
-        expect(ageClassification(24)).toBe('Дитинство');
-        expect(ageClassification(24.01)).toBe('Молодість');
-        expect(ageClassification(44)).toBe('Молодість');
-        expect(ageClassification(44.01)).toBe('Зрілість');
-        expect(ageClassification(65)).toBe('Зрілість');
-        expect(ageClassification(65.1)).toBe('Старість');
-        expect(ageClassification(75)).toBe('Старість');
-        expect(ageClassification(75.01)).toBe('Довголіття');
-        expect(ageClassification(90)).toBe('Довголіття');
-        expect(ageClassification(90.01)).toBe('Рекорд');
-        expect(ageClassification(122)).toBe('Рекорд');
-        expect(ageClassification(122.01)).toBe(null);
-        expect(ageClassification(150)).toBe(null);
-        expect(ageClassification('asd')).toBe(null);
-        expect(ageClassification('')).toBe(null);
-        expect(ageClassification([12, 5])).toBe(null);
-        expect(ageClassification(true)).toBe(null);
-        expect(ageClassification(false)).toBe(null);
+describe('ageClassification function', () => {
+    it('Should display null type with negative number.', () => {
+        expect(ageClassification(-1)).toBeNull();
     });
-
+    it('Should display null type with 0 number.', () => {
+        expect(ageClassification(0)).toBeNull();
+    });
+    it('Should display "Дитинство" in age period from 0 (not included) to 24. ', () => {
+        expect(ageClassification(1)).toBe('Дитинство');
+    });
+    it('Should display "Дитинство" in age period from 0 (not included) to 24. ', () => {
+        expect(ageClassification(24)).toBe('Дитинство');
+    });
+    it('Should display "Молодість" in age period from 24 (not included) to 44. ', () => {
+        expect(ageClassification(24.01)).toBe('Молодість');
+    });
+    it('Should display "Молодість" in age period from 24 (not included) to 44. ', () => {
+        expect(ageClassification(44)).toBe('Молодість');
+    });
+    it('Should display "Зрілість" in age period from 44 (not included) to 65. ', () => {
+        expect(ageClassification(44.01)).toBe('Зрілість');
+    });
+    it('Should display "Зрілість" in age period from 44 (not included) to 65. ', () => {
+        expect(ageClassification(65)).toBe('Зрілість');
+    });
+    it('Should display "Старість" in age period from 65 (not included) to 75. ', () => {
+        expect(ageClassification(65.1)).toBe('Старість');
+    });
+    it('Should display "Старість" in age period from 65 (not included) to 75. ', () => {
+        expect(ageClassification(75)).toBe('Старість');
+    });
+    it('Should display "Довголіття" in age period from 75 (not included) to 90. ', () => {
+        expect(ageClassification(75.01)).toBe('Довголіття');
+    });
+    it('Should display "Довголіття" in age period from 75 (not included) to 90. ', () => {
+        expect(ageClassification(90)).toBe('Довголіття');
+    });
+    it('Should display "Рекорд" in age period from 90 (not included) to 122. ', () => {
+        expect(ageClassification(90.01)).toBe('Рекорд');
+    });
+    it('Should display "Рекорд" in age period from 90 (not included) to 122. ', () => {
+        expect(ageClassification(122)).toBe('Рекорд');
+    });
+    it('Should display "null" in age period over 122 (not included). ', () => {
+        expect(ageClassification(122.01)).toBeNull();
+    });
+    it('Should display "null" in age period over 122 (not included). ', () => {
+        expect(ageClassification(150)).toBeNull();
+    });
+    it('Should display "null" if entered string type. ', () => {
+        expect(ageClassification('asd')).toBeNull();
+    });
+    it('Should display "null" if parameters are empty. ', () => {
+        expect(ageClassification('')).toBeNull();
+    });
+    it('Should display "null" if entered Array. ', () => {
+        expect(ageClassification([12, 5])).toBeNull();
+    });
+    it('Should display "null" if entered Boolean type. ', () => {
+        expect(ageClassification(true)).toBeNull();
+    });
+    it('Should display "null" if entered Boolean type. ', () => {
+        expect(ageClassification(false)).toBeNull();
+    });
 })
-describe('weekFn', () => {
-    it('Should display day of the week, depending of number', () => {
+
+describe('weekFn function', () => {
+    it('Should display "Понеділок" when enter number 1.', () => {
         expect(weekFn(1)).toBe('Понеділок');
+    });
+    it('Should display "Вівторок" when enter number 2.', () => {
         expect(weekFn(2)).toBe('Вівторок');
+    });
+    it('Should display "Середа" when enter number 3.', () => {
         expect(weekFn(3)).toBe('Середа');
+    });
+    it('Should display "Четвер" when enter number 4.', () => {
         expect(weekFn(4)).toBe('Четвер');
+    });
+    it('Should display "П\'ятниця" when enter number 5.', () => {
         expect(weekFn(5)).toBe('П\'ятниця');
+    });
+    it('Should display "Субота" when enter number 6.', () => {
         expect(weekFn(6)).toBe('Субота');
+    });
+    it('Should display "Неділя" when enter number 7.', () => {
         expect(weekFn(7)).toBe('Неділя');
-        expect(weekFn(10)).toBe(null);
-        expect(weekFn(1.5)).toBe(null);
-        expect(weekFn(0)).toBe(null);
-        expect(weekFn(-1)).toBe(null);
-        expect(weekFn('2')).toBe(null);
-        expect(weekFn([2, 5])).toBe(null);
-        expect(weekFn(0)).toBe(null);
-        expect(weekFn(false)).toBe(null);
-        expect(weekFn(true)).toBe(null);
+    });
+    it('Should display "null" when enter numbers of any other diapason.', () => {
+        expect(weekFn(10)).toBeNull();
+    });
+    it('Should display "null" when enter numbers of any other diapason.', () => {
+        expect(weekFn(1.5)).toBeNull();
+    });
+    it('Should display "null" when enter numbers of any other diapason.', () => {
+        expect(weekFn(0)).toBeNull();
+    });
+    it('Should display "null" when enter numbers of any other diapason.', () => {
+        expect(weekFn(-1)).toBeNull();
+    });
+    it('Should display "null" when enter parameters of type "string".', () => {
+        expect(weekFn('2')).toBeNull();
+    });
+    it('Should display "null" when enter parameters of type "array".', () => {
+        expect(weekFn([2, 5])).toBeNull();
+    });
+    it('Should display "null" when enter parameters of type "boolean".', () => {
+        expect(weekFn(false)).toBeNull();
+    });
+    it('Should display "null" when enter parameters of type "boolean".', () => {
+        expect(weekFn(true)).toBeNull();
     })
 })
 
