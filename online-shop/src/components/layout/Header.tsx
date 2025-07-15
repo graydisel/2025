1872401fe1d6d566b067df4060d8a2a153786f78
Babanenkov Sergii <graydisel@gmail.com>
2025-07-15 18:46:46 +0300

    Complete Front Header

--- online-shop/src/components/layout/Header.tsx
+++ online-shop/src/components/layout/Header.tsx
@@ -1,9 +1,13 @@
-
+import "../../css/Header.css"
 
 const Header = () => {
     return (
         <header>
-            <h1>Header</h1>
+            <div className={"logo-container"}>
+                <img className={"header-logo"} src="./src/assets/images/logo.svg" alt="Logo"/>
+                <img className={"header-logo-name"} src="./src/assets/images/logo_name.svg" alt="Logo Name"/>
+            </div>
+            <img className={"header-image"} src="./src/assets/images/header/bookshelf.svg" alt="Bookshelf" />
         </header>
     )
 }
